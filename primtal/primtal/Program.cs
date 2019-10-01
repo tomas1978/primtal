c5dd9ee8b71bf07a99d2148c23bd5890809013fb
Tomas Nilsson <tomas.nilsson78@gmail.com>
2019-10-01 10:16:08 +0200

    Implemented and tested method for checking if an integer is a prime number.

---
+++ primtal/primtal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primtal
+{
+    class Program
+    {
+        public static bool IsPrime(int tal)
+        {
+            bool isPrime = true;
+            for(int i=2;i<Math.Sqrt(tal)+1;i++)
+            {
+                if(tal%i==0)
+                {
+                    isPrime = false;
+                }
+            }
+            return isPrime;
+        }
+        static void Main(string[] args)
+        {
+            int tal = 23;
+            Console.WriteLine(tal + " " + IsPrime(tal));
+        }
+    }
+}
